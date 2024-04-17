f9e951ca3a604a117635c0c26034d5800a389215
Krishanu321 <krishanusantra345@gmail.com>
2024-04-17 12:53:55 +0530

    Xoring and Clearing

---
+++ 14.04.2024.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    void printArr(int n, int arr[])
+    {
+        // Your code for printing array here
+    }
+
+    void setToZero(int n, int arr[])
+    {
+        // Use memset to set arr to zero
+    }
+
+    void xor1ToN(int n, int arr[])
+    {
+        // Xor arr[i]^i
+    }
+};
